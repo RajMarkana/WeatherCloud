8218922002abde94ad3b410840be6df8528c07aa
Raj Markana <rajmarkana1324@gmail.com>
2024-05-07 14:05:35 +0530

    Fix: Api Issue

--- src/App.jsx
+++ src/App.jsx
@@ -23,8 +23,8 @@ const App = () => {
   };
 
   function getWeather(city) {
-    const api = import.meta.env.VITE_REACT_APP_WEATHER_KEY;
-    // const api = process.env.VITE_REACT_APP_WEATHER_KEY;
+    // const api = import.meta.env.VITE_REACT_APP_WEATHER_KEY;
+    const api = process.env.VITE_REACT_APP_WEATHER_KEY;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}`;
 
     fetch(url)
